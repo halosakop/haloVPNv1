01b0934c3ffb7a0edbfe598243c7c7227cde2a47
halosakop <150012023+halosakop@users.noreply.github.com>
2026-02-13 12:07:13 +0100

    sifrovanie

--- client.go
+++ client.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"github.com/songgao/water"
 	"log"
 	"net"
-
-	"github.com/songgao/water"
 )
 
-const mtu = 1500
+const (
+	mtu = 1400 //  velkost paketu
+	key = "\xb5\x2c\x81\xa0\x1d\x5f\xff\x9a\xac\xbb\xe3\x71\x99\x18\x2b\x71\x97\x5a\x00\x66\xe7\x50\x39\x2c\x65\x97\x25\xab\xd6\xc6\x53\x3b"
+)
 
 func isIPv4(pkt []byte) bool {
 	return len(pkt) >= 20 && pkt[0]>>4 == 4
@@ -21,31 +25,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("TUN:", ifce.Name())
+	log.Println("nazov TUN interface:", ifce.Name())
 
-	server, _ := net.ResolveUDPAddr("udp", "91.99.203.50:51820")
-	conn, err := net.DialUDP("udp", nil, server)
+	server, _ := net.ResolveUDPAddr("udp", "91.99.203.50:51820") // preloží IP adresu a port servera
+	conn, err := net.DialUDP("udp", nil, server)                 //vytvorenie UDP spojenia na server
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// UDP -> TUN
-	go func() {
-		buf := make([]byte, mtu)
+	block, _ := aes.NewCipher([]byte(key)) //sifrovanie pomocou AES
+	aesgcm, _ := cipher.NewGCM(block)
+	nonce := make([]byte, aesgcm.NonceSize())
+
+	// UDP -> TUN (dekriptovanie a zapis do TUN)
+	go func() { //spustenie gorutiny pre prijimanie UDP paketov
+		buf := make([]byte, mtu+100)
 		for {
 			n, err := conn.Read(buf)
 			if err != nil {
 				continue
 			}
-			if !isIPv4(buf[:n]) {
+			plainText, err := aesgcm.Open(nil, nonce, buf[:n], nil)
+			if err != nil {
 				continue
 			}
-			ifce.Write(buf[:n])
+			if !isIPv4(plainText) {
+				continue
+			}
+
+			ifce.Write(plainText)
 		}
 	}()
 
-	// TUN -> UDP
-	pkt := make([]byte, mtu)
+	// TUN -> UDP (kriptovanie a posielanie na server)
+	pkt := make([]byte, mtu+100)
 	for {
 		n, err := ifce.Read(pkt)
 		if err != nil {
@@ -54,6 +67,8 @@ func main() {
 		if !isIPv4(pkt[:n]) {
 			continue
 		}
-		conn.Write(pkt[:n])
+
+		cipherText := aesgcm.Seal(nil, nonce, pkt[:n], nil)
+		conn.Write(cipherText)
 	}
 }
--- server.go
+++ server.go
@@ -1,65 +1,82 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"github.com/songgao/water"
 	"log"
 	"net"
-
-	"github.com/songgao/water"
 )
 
-const mtu = 1500
-
-func isIPv4(pkt []byte) bool {
-	return len(pkt) >= 20 && pkt[0]>>4 == 4
-}
+const (
+	mtu = 1400
+	key = "\xb5\x2c\x81\xa0\x1d\x5f\xff\x9a\xac\xbb\xe3\x71\x99\x18\x2b\x71\x97\x5a\x00\x66\xe7\x50\x39\x2c\x65\x97\x25\xab\xd6\xc6\x53\x3b"
+)
 
 func main() {
-	cfg := water.Config{
-		DeviceType: water.TUN,
-	}
+	cfg := water.Config{DeviceType: water.TUN} // vytvorenie TUN interface
 	ifce, err := water.New(cfg)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Nepodarilo sa vytvorit TUN adapter:", err)
 	}
-	log.Println("TUN:", ifce.Name())
+	log.Println("TUN interface:", ifce.Name())
 
-	addr := net.UDPAddr{Port: 51820}
-	udp, err := net.ListenUDP("udp", &addr)
-	if err != nil {
-		log.Fatal(err)
+	addr := net.UDPAddr{ // pocuvanie na IPv4 UDP port 51820
+		IP:   net.IPv4zero, // 0.0.0.0 (vsetky IPv4 adresy)
+		Port: 51820,
 	}
+	udp, err := net.ListenUDP("udp4", &addr)
+	if err != nil {
+		log.Fatal("Neprebehlo pocuvanie na UDP:", err)
+	}
+	log.Println("Pocuvanie na UDP:", addr.Port)
+
+	block, _ := aes.NewCipher([]byte(key)) //kryptovanie pomocou AES, vytvorenie cipher bloku s klucom
+	aesgcm, _ := cipher.NewGCM(block)
+	nonce := make([]byte, aesgcm.NonceSize())
 
 	var client *net.UDPAddr
 
-	// UDP -> TUN
+	// UDP -> TUN (dekriptovanie a zapis do TUN)
 	go func() {
-		buf := make([]byte, mtu)
+		buf := make([]byte, mtu+100)
 		for {
 			n, raddr, err := udp.ReadFromUDP(buf)
+			if err != nil {
+				log.Println("UDP chyba:", err)
+				continue
+			}
+
+			// Registrovanie prveho klienta, ktory posle paket
+			if client == nil {
+				client = raddr
+				log.Println("Registrovany client:", client)
+			}
+
+			// prepossielanie paketov do TUN interface
+			plainText, err := aesgcm.Open(nil, nonce, buf[:n], nil)
 			if err != nil {
 				continue
 			}
 
-			if client == nil {
-				client = raddr
-				log.Println("Registered client:", client)
-			}
-
-			if !isIPv4(buf[:n]) {
-				continue
-			}
-
-			ifce.Write(buf[:n])
+			ifce.Write(plainText)
 		}
 	}()
 
-	// TUN -> UDP
-	pkt := make([]byte, mtu)
+	// TUN -> UDP (kriptovanie a posielanie na klienta)
+	pkt := make([]byte, mtu+100)
 	for {
 		n, err := ifce.Read(pkt)
-		if err != nil || client == nil {
+		if err != nil {
+			log.Println("TUN chyba:", err)
 			continue
 		}
-		udp.WriteToUDP(pkt[:n], client)
+		if client == nil {
+			// zahodi paket, kym sa nezaregistruje klient
+			continue
+		}
+
+		cipherText := aesgcm.Seal(nil, nonce, pkt[:n], nil)
+		udp.WriteToUDP(cipherText, client)
 	}
 }
